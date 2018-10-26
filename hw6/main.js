d61265934a60a486b6c00a10cd661f5fb19c5c87
ffrfaye <ffrances812@gmail.com>
2018-10-25 20:18:34 -0400

    Shopping cart mostly functional!

--- hw6/main.js
+++ hw6/main.js
@@ -45,45 +45,18 @@ $(document).ready(function(){
 /*** Shopping cart functions ***/
 
 /** Object constructor **/
-function bun(selection, glazing, quantity) {
+function bun(selection, glazing, quantity, index) {
   this.name = arrayNames[selection];
   this.glazing = arrayGlazing[glazing];
   this.imgsrc = arrayImgsrc[selection];
   this.price = arrayPrice[selection];
   this.priceNum = arrayPriceNum[selection];
   this.quantity = quantity;
+  this.index = index;
 }
 
-$(document).ready(function(){
-
-  var cartItemsTable = document.getElementById("cartItems");
-
-  if (localStorage.length>0){
-    for(i=0;i<localStorage.length;i++){
-      addBun = JSON.parse(localStorage.getItem(i));
-      console.log(addBun);
-      console.log(addBun.imgsrc);
-      createItemRow(cartItemsTable, addBun);
-    }
-  }
-
-  $("#addToCartButton").click(function(){
-    var selection = $('.active').attr("data-flavor");
-    var glazing = $('.glazeActive').attr("data-glazing");
-    var quantity = parseInt($('.quantActive').attr("data-quantity"));
-
-    cartCount = cartCount+quantity;
-    $("#cartCount").html(cartCount);
-
-    myBun = new bun(selection, glazing, quantity);
-    localStorage.setItem(savedBun,JSON.stringify(myBun));
-    savedBun = savedBun+1;
-    hasBun = true;
-  })
-})
-
 function createItemRow(myTable, bunInfo){
-  var row = myTable.insertRow(0);
+  var row = myTable.insertRow(-1);
   var deleteCell = row.insertCell(0);
   var imageCell = row.insertCell(1);
   var textCell = row.insertCell(2);
@@ -91,9 +64,12 @@ function createItemRow(myTable, bunInfo){
   var totalCell = row.insertCell(4);
 
   var deleteButton = document.createElement('button');
+  deleteButton.setAttribute("id", "deleteButton");
+  deleteButton.setAttribute("onclick","deleteRow(this)")
+  deleteButton.setAttribute("index",bunInfo.index);
   deleteButton.innerHTML = "X";
   deleteCell.appendChild(deleteButton);
-  deleteCell.style.width="10%";
+  deleteCell.style.width="5%";
 
   var bunImg = document.createElement('img');
   bunImg.src = bunInfo.imgsrc;
@@ -103,12 +79,74 @@ function createItemRow(myTable, bunInfo){
   imageCell.style.width="30%";
 
   textCell.innerHTML = bunInfo.name+" Cinnamon Bun<br />Glazing: "+bunInfo.glazing
-  +"<br /><br /><br />"+bunInfo.price;
-  textCell.style.width="30%";
+  +"<br /><br />"+bunInfo.price;
+  textCell.style.width="35%";
 
+  quantityCell.setAttribute("class","itemQuant");
+  quantityCell.setAttribute("data-quant",bunInfo.quantity);
   quantityCell.innerHTML = "Qty: "+bunInfo.quantity;
   quantityCell.style.width="15%";
 
+  totalCell.setAttribute("class","itemTotalPrice");
+  totalCell.setAttribute("data-price",bunInfo.priceNum*bunInfo.quantity);
   totalCell.innerHTML = "$"+bunInfo.priceNum*bunInfo.quantity;
   totalCell.style.width="15%";
+
+}
+
+function updatePage(){
+  var totalPrice = 0;
+  var listPrice = document.getElementsByClassName("itemTotalPrice");
+  console.log(listPrice)
+  for (var i = 0; i < listPrice.length; i++ ){
+    totalPrice += parseInt(listPrice[i].getAttribute("data-price"));
+  }
+
+  var totalCount = 0;
+  var listCount = document.getElementsByClassName("itemQuant");
+  for (var i = 0; i < listCount.length; i++ ){
+    totalCount += parseInt(listCount[i].getAttribute("data-quant"));
+  }
+
+  $("#cartPrice").html("$"+totalPrice);
+  $("#totalPrice").html("$"+totalPrice);
+  $("#cartCount").html(totalCount);
+}
+
+/** The function that runs all the functions! **/
+$(document).ready(function(){
+
+  var cartItemsTable = document.getElementById("cartItems");
+
+  if (localStorage.length>0){
+    for(i=0;i<localStorage.length;i++){
+      addBun = JSON.parse(localStorage.getItem(i));
+      createItemRow(cartItemsTable, addBun);
+    }
+    updatePage();
+  }
+
+  $("#addToCartButton").click(function(){
+    var selection = $('.active').attr("data-flavor");
+    var glazing = $('.glazeActive').attr("data-glazing");
+    var quantity = parseInt($('.quantActive').attr("data-quantity"));
+    var index = savedBun;
+
+    cartCount = cartCount+quantity;
+    $("#cartCount").html(cartCount);
+
+    myBun = new bun(selection, glazing, quantity, index);
+    localStorage.setItem(savedBun,JSON.stringify(myBun));
+    savedBun = savedBun+1;
+    hasBun = true;
+  })
+})
+
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    var index = $(r).attr("index");
+    console.log(index);
+    document.getElementById("cartItems").deleteRow(i);
+    localStorage.removeItem(index);
+    updatePage();
 }
